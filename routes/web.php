527d43dc626dbb3cb3fbe3064a9e07bc2eeaee39
buixuanxepdz <buixuanxep@gmail.com>
2021-05-12 22:01:02 +0700

    btbuoi2

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,42 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test', function () {
-    return view('test');
-});
+Route::prefix('task')->group(function () {
+    Route::get('/', function () {
+     return view('tasks.index');
+     });
+ 
+     Route::get('/edit/{id}', function ($id) {
+     echo 'edit task id = '.$id;
+     });
+ 
+     Route::get('/complete/{id}', function ($id) {
+     echo 'complete task id = '.$id;
+     })->name('task.complete');
+ 
+     Route::get('/reset/{id}', function ($id) {
+     echo 'reset task id = '.$id;
+     })->name('task.reset');
+ 
+     Route::delete('/delete/{id}', function ($id) {
+     echo 'delete task id = '.$id;
+     })->name('task.delete');
+ });
+ 
+ 
+ Route::get('/user/{id}/post/{post}', function($id, $idPost) {
+     return "This is post $idPost of user $id"; 
+ });
+ 
+ Route::get('user/{id?}', function($id = null) {
+     if ($id == null) {
+         return 'List users';
+     }
+     
+     return "User $id";
+ });
+ 
+ Route::post('/create', function () {
+     echo 'create task';
+ });
 
